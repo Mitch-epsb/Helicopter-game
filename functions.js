63d4c40c4100e8d04d67e3a484f6b863a23256fe
Mitch-epsb <m.patterson2@share.epsb.ca>
2025-01-15 14:14:12 -0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,219 @@
+// FUNCTIONS
+
+// Draw Start Screen
+function drawStart() {
+  // Background
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+  // Green Bars
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, cnv.width, 50);
+  ctx.fillRect(0, cnv.height - 50, cnv.width, 50);
+
+  // Green Bar Text
+  ctx.font = "30px Consolas";
+  ctx.fillStyle = "black";
+  ctx.fillText("HELICOPTER GAME", 25, 35);
+  ctx.fillText("DISTANCE: 0", 25, cnv.height - 15);
+  ctx.fillText("BEST:" + best, cnv.width - 250, cnv.height - 15);
+
+  // Helicopter
+  ctx.drawImage(heliImg, heli.x, heli.y);
+
+  // Start Text
+  ctx.font = "40px Consolas";
+  ctx.fillStyle = "lightblue";
+  ctx.fillText("CLICK TO START", 350, 285);
+
+  ctx.font = "25px Consolas";
+  ctx.fillText("CLICK AND HOLD LEFT MOUSE BUTTON TO GO UP", 100, 450);
+  ctx.fillText("RELEASE TO GO DOWN", 415, 480);
+}
+
+function rungame() {
+  // LOGIC
+  moveHeli();
+  movewalls();
+  collCheck();
+  distance();
+  //GAME
+  drawGame();
+}
+function moveHeli() {
+  //Apply Grav
+  heli.speed += heli.accel;
+  if (mouseispressed) {
+    heli.speed += -0.6;
+  }
+
+  //Max speed
+  if (heli.speed > 6) {
+    heli.speed = 6;
+  } else if (heli.speed < -6) {
+    heli.speed = -6;
+  }
+  //Move Heli
+  heli.y += heli.speed;
+}
+function distance() {
+  dis += 1;
+  if (dis > best) {
+    best = dis;
+  }
+}
+
+function movewalls() {
+  //wall1
+  wall1.x += -wall1.s;
+  wall1.s += 0.001;
+  if (wall1.x + wall1.w < 0) {
+    wall1.x = wall3.x + 500;
+    wall1.y = Math.random() * 300 + 100;
+  }
+  //wall2
+  wall2.x += -wall2.s;
+  wall2.s += 0.001;
+  if (wall2.x + wall2.w < 0) {
+    wall2.x = wall1.x + 500;
+    wall2.y = Math.random() * 300 + 100;
+  }
+  //wall3
+  wall3.x += -wall3.s;
+  wall3.s += 0.001;
+  if (wall3.x + wall3.w < 0) {
+    wall3.x = wall2.x + 500;
+    wall3.y = Math.random() * 300 + 100;
+  }
+}
+function collCheck() {
+  if (heli.y < 50 || heli.y > cnv.height - 90) {
+    gameover();
+  }
+  if (
+    (heli.y + heli.h > wall1.y &&
+      heli.y < wall1.y + wall1.h &&
+      heli.x + heli.w > wall1.x &&
+      heli.x < wall1.x + wall1.w) ||
+    (heli.y + heli.h > wall3.y &&
+      heli.y < wall3.y + wall3.h &&
+      heli.x + heli.w > wall3.x &&
+      heli.x < wall3.x + wall3.w) ||
+    (heli.y + heli.h > wall2.y &&
+      heli.y < wall2.y + wall2.h &&
+      heli.x + heli.w > wall2.x &&
+      heli.x < wall2.x + wall2.w)
+  ) {
+    gameover();
+  }
+}
+function gameover() {
+  explo.play();
+  state = "gameover";
+
+  setTimeout(reset, 2000);
+}
+// Draw Game Elements
+function drawGame() {
+  // Background
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+  // Green Bars
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, cnv.width, 50);
+  ctx.fillRect(0, cnv.height - 50, cnv.width, 50);
+
+  // Green Bar Text
+  ctx.font = "30px Consolas";
+  ctx.fillStyle = "black";
+  ctx.fillText("HELICOPTER GAME", 25, 35);
+  ctx.fillText("DISTANCE:" + dis, 25, cnv.height - 15);
+  ctx.fillText("BEST:" + best, cnv.width - 250, cnv.height - 15);
+
+  // Helicopter
+  ctx.drawImage(heliImg, heli.x, heli.y);
+
+  // Draw Wall 1
+  ctx.fillStyle = "green";
+  ctx.fillRect(wall1.x, wall1.y, wall1.w, wall1.h);
+  ctx.fillRect(wall2.x, wall2.y, wall2.w, wall2.h);
+  ctx.fillRect(wall3.x, wall3.y, wall3.w, wall3.h);
+}
+
+// Draw Game Over Screen
+function drawGameOver() {
+  // Background
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+  // Green Bars
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, cnv.width, 50);
+  ctx.fillRect(0, cnv.height - 50, cnv.width, 50);
+
+  // Green Bar Text
+  ctx.font = "30px Consolas";
+  ctx.fillStyle = "black";
+  ctx.fillText("HELICOPTER GAME", 25, 35);
+  ctx.fillText("DISTANCE:" + dis, 25, cnv.height - 15);
+  ctx.fillText("BEST:" + best, cnv.width - 250, cnv.height - 15);
+
+  // Helicopter
+  ctx.drawImage(heliImg, heli.x, heli.y);
+
+  // Draw Wall 1
+  ctx.fillStyle = "green";
+  ctx.fillRect(wall1.x, wall1.y, wall1.w, wall1.h);
+  ctx.fillRect(wall2.x, wall2.y, wall2.w, wall2.h);
+  ctx.fillRect(wall3.x, wall3.y, wall3.w, wall3.h);
+
+  // Circle around Helicopter
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.arc(heli.x + 40, heli.y + 20, 60, 0, 2 * Math.PI);
+  ctx.stroke();
+
+  // Game Over Text
+  ctx.font = "40px Consolas";
+  ctx.fillStyle = "lightblue";
+  ctx.fillText("GAME OVER", 350, 285);
+}
+
+// Helper Functions
+function reset() {
+  state = "start";
+  heli = {
+    x: 200,
+    y: 250,
+    w: 80,
+    h: 40,
+    speed: 0,
+    accel: 0.3,
+  };
+  wall1 = {
+    x: cnv.width,
+    y: Math.random() * 300 + 100,
+    w: 50,
+    h: 100,
+    s: 3,
+  };
+  wall2 = {
+    x: cnv.width + 500,
+    y: Math.random() * 300 + 100,
+    w: 50,
+    h: 100,
+    s: 3,
+  };
+  wall3 = {
+    x: cnv.width + 1000,
+    y: Math.random() * 300 + 100,
+    w: 50,
+    h: 100,
+    s: 3,
+  };
+  dis = 0;
+}
+
+function drawWalls() {}
---
+++ main.js
@@ -0,0 +1,55 @@
+// Helicopter Game Start
+
+// Set up canvas and graphics context
+let cnv = document.getElementById("my-canvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+// Global Variables
+let heliImg = document.createElement("img");
+heliImg.src = "img/heliBlueTransparent.png";
+let explo = document.createElement("audio");
+explo.src = "sound/explosion.wav";
+let propeller = document.createElement("audio");
+propeller.src = "sound/propeller.wav";
+let mouseispressed = false;
+let best = 0;
+// reset
+let state;
+let heli;
+let wall1, wall2, wall3;
+let dis;
+reset();
+
+// Draw Function
+window.addEventListener("load", draw);
+
+function draw() {
+  if (state == "start") {
+    drawStart();
+  } else if (state == "gameon") {
+    rungame();
+  } else if (state == "gameover") {
+    drawGameOver();
+  }
+
+  // Request Animation Frame
+  requestAnimationFrame(draw);
+}
+
+// EVENTS
+document.addEventListener("mousedown", mousedownHandler);
+document.addEventListener("mouseup", mouseupHandler);
+
+function mousedownHandler() {
+  mouseispressed = true;
+  if (state == "start") {
+    state = "gameon";
+  }
+  propeller.play();
+}
+
+function mouseupHandler() {
+  mouseispressed = false;
+}
